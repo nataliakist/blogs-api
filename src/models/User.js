217047850f7db476a8802df88b71c6a55a64602a
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-07-03 21:35:04 -0300

    Requisito 02
    Criando o model User.js

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users',
+  });
+
+  User.associate = (models) => {
+    User.hasOne(models.Account,
+      { foreignKey: 'user_id', as: 'users' });
+  };
+
+  return User;
+}
